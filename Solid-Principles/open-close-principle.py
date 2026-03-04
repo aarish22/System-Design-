3daa277f71fae092479663c51072ac6197c90f46
aarish <aarishalam22@gmail.com>
2026-03-04 12:35:18 +0530

    restructure folder

---
+++ Solid-Principles/open-close-principle.py
@@ -0,0 +1,60 @@
+from abc import abstractmethod,ABC
+class Product:
+  def __init__(self,name,price):
+    self.name = name
+    self.price = price
+
+class ShoppingCart:
+  def __init__(self):
+    self.totalProducts = []
+    
+  def pushProduct(self,product):
+    self.totalProducts.append(product)
+    
+  def getPrice(self):
+    return sum(product.price for product in self.totalProducts)
+  
+  
+class Invoice:
+  def __init__(self,cart):
+    self.cart = cart
+    
+  def generate(self):
+    return f"Rs. {self.cart.getPrice()} - Invoice generated "
+    
+  
+class SavetoDB(ABC):
+  
+  @abstractmethod
+  def save(self):
+    pass
+  
+class SavetoMongo(SavetoDB):
+  def __init__(self,cart):
+    self.cart = cart
+  
+  def save(self):
+    return f"{len(self.cart.totalProducts)} products saved to MongoDb"
+
+class SavetoPostgres(SavetoDB):
+  def __init__(self,cart):
+    self.cart = cart
+  
+  def save(self):
+    return f"{len(self.cart.totalProducts)} products saved to Postgres"
+
+
+p1 = Product("Laptop", 50000)
+p2 = Product("Phone", 15000)
+
+cart = ShoppingCart()
+cart.pushProduct(p1)
+cart.pushProduct(p2)
+
+invoice = Invoice(cart)
+print(invoice.generate())
+
+db = SavetoMongo(cart)
+db1 = SavetoPostgres(cart)
+print(db.save())
+print(db1.save())
